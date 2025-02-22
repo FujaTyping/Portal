aaf4a7bd0b9441b1d62d0683646d6c14829e82a7
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-02-22 23:47:12 +0700

    Full UI Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import { Open_Sans } from "next/font/google";
-import Banner from "@/app/assets/media/Banner.png"
+import Banner from "@/app/assets/media/Banner.webp";
+import { Github, Twitter, Mail, Linkedin } from 'lucide-react';
 
 const OS = Open_Sans({
   subsets: ['latin'],
@@ -9,27 +11,27 @@ export default function Home() {
   return (
     <>
       <div className="h-screen w-full flex flex-col items-center justify-center p-3">
-        <div className="bg-gradient-to-r from-red-600 to-red-800 max-w-6xl w-full rounded-lg">
+        <div className="bg-gradient-to-r from-red-600 to-red-800 max-w-5xl w-full rounded-lg swing-in-top-fwd">
           <img
             className="w-full max-h-72 rounded-t-lg object-cover"
             src={Banner.src}
             alt="Banner"
           />
-          <div className="flex flex-col md:flex-row gap-6 place-content-between px-6 py-12 text-white">
+          <div className="flex flex-col md:flex-row gap-6 place-content-between px-6 py-12 text-white border-t-4 border-red-900">
             <div>
               <h1 className="font-bold text-4xl md:text-5xl">FujaTyping</h1>
-              <p className="max-w-md mt-3">A normal guy</p>
+              <p className="max-w-md mt-3">3D modeler, Web developer, and Roblox scripter, Anime lover and music vibe hunter</p>
             </div>
             <div className={`${OS.className} flex flex-col w-full md:ml-6 md:flex-row items-center justify-center gap-5`}>
-              <button className="w-full">Github</button>
-              <button className="w-full">Twitter</button>
-              <button className="w-full">Mail</button>
-              <button className="w-full">Discord</button>
+              <Link href={"https://github.com/FujaTyping"} className="w-full flex items-center justify-center gap-2"><Github size={18} /> Github</Link>
+              <Link href={"https://x.com/FujaTyping"} className="w-full flex items-center justify-center gap-2"><Twitter size={18} /> Twitter</Link>
+              <Link href={"mailto:contact@siraphop.me"} className="w-full flex items-center justify-center gap-2"><Mail size={18} /> Mail</Link>
+              <Link href={"https://www.linkedin.com/in/siraphop-sukchu/"} className="w-full flex items-center justify-center gap-2"><Linkedin size={18} /> Linkedin</Link>
             </div>
           </div>
         </div>
         <div className="absolute bottom-10 flex gap-2">
-          <div className="w-8 h-8 bg-red-600 rounded-full"></div>
+          <div className="w-9 h-9 bg-red-600 rounded-full"></div>
           <p className="bg-red-600 text-white px-3 py-1 rounded-full text-xl">
             siraphop.me
           </p>
