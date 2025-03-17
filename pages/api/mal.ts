d08fdba2ac976e0d240341bd5233d5c108c9311c
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-03-17 20:35:58 +0700

    Move fetch to server side

---
+++ pages/api/mal.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const MALCID = process.env.NEXT_PUBLIC_MAL_CLIENT_ID;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const response = await fetch(
+      "https://api.myanimelist.net/v2/users/FujaTyping/animelist?fields=list_status&limit=10&sort=list_updated_at",
+      {
+        headers: {
+          "X-MAL-CLIENT-ID": `${MALCID}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      return res
+        .status(response.status)
+        .json({ error: "Failed to fetch data" });
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
