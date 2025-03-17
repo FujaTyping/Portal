59a720322b43b1eba054d58837fcaa36ba3fe252
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-03-17 17:51:07 +0700

    Add MAL

---
+++ src/app/components/Myanimelist.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Jaini } from "next/font/google";
+
+import "swiper/css";
+
+import { Autoplay } from "swiper/modules";
+import Link from "next/link";
+import { MoonStar, ListCollapse } from "lucide-react";
+
+const MALCID = process.env.NEXT_PUBLIC_MAL_CLIENT_ID;
+
+const JN = Jaini({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+interface Anime {
+  node: {
+    id: number;
+    title: string;
+    main_picture: {
+      medium: string;
+      large: string;
+    };
+  };
+  list_status: {
+    status: string;
+    score: number;
+    num_episodes_watched: number;
+    is_rewatching: boolean;
+    updated_at: string;
+    finish_date?: string;
+    start_date?: string;
+  };
+}
+
+function Myanimelist() {
+  const [data, setData] = useState<Anime[]>([]);
+  const [fetchStatus, setFetchStatus] = useState(true);
+  const [errorMsg, setErrorMsg] = useState("");
+  const options = {
+    method: "GET",
+    url: "https://api.myanimelist.net/v2/users/FujaTyping/animelist",
+    params: { fields: "list_status", limit: "20", sort: "list_updated_at" },
+    headers: { "X-MAL-CLIENT-ID": MALCID },
+  };
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((response) => {
+        setData(response.data.data);
+        setFetchStatus(false);
+      })
+      .catch((error) => {
+        setErrorMsg(error.message);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="bg-gradient-to-r from-red-600 to-red-700 max-w-5xl w-full mt-7 text-white rounded-lg">
+        <div className="p-6">
+          <h1 className="text-3xl font-bold flex items-center gap-3">
+            <MoonStar size={30} /> My anime list
+          </h1>
+          <h2>
+            View more on{" "}
+            <Link href={"https://myanimelist.net/animelist/FujaTyping"}>
+              <b>myanimelist.net</b>
+            </Link>
+          </h2>
+        </div>
+        <div className="pb-8 mx-auto px-5">
+          {fetchStatus == true ? (
+            <>
+              <div className="my-5 flex flex-col gap-3">
+                <div className="loader mx-auto"></div>
+                {errorMsg != "" && (
+                  <>
+                    <p className="text-center text-sm">{errorMsg}</p>
+                  </>
+                )}
+              </div>
+            </>
+          ) : (
+            <>
+              <Swiper
+                spaceBetween={20}
+                slidesPerView={2}
+                loop={true}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                breakpoints={{
+                  768: {
+                    slidesPerView: 3,
+                  },
+                  1024: {
+                    slidesPerView: 4,
+                  },
+                }}
+                modules={[Autoplay]}
+              >
+                {data.map((anime, index) => (
+                  <SwiperSlide key={index}>
+                    <div className={`${JN.className} w-54`}>
+                      <img
+                        src={anime.node.main_picture.large}
+                        alt={anime.node.title}
+                        className="object-cover rounded-t-lg"
+                      />
+                      <div className="bg-red-700 p-4 rounded-b-lg">
+                        <h1 className="text-lg">{anime.node.title}</h1>
+                        <div className="flex place-content-between mt-2">
+                          <h2 className="flex items-center gap-2">
+                            <ListCollapse size={20} />
+                            {anime.list_status.status
+                              .replace(/_/g, " ")
+                              .replace(/\b\w/g, (char) => char.toUpperCase())}
+                          </h2>
+                          <h2>My Score : {anime.list_status.score}</h2>
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Myanimelist;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,30 @@
 import Link from "next/link";
 import { Open_Sans } from "next/font/google";
 import Banner from "@/app/assets/media/Banner.webp";
-import { Github, Twitter, Mail, Linkedin, Sparkle } from 'lucide-react';
+import { Github, Twitter, Mail, Linkedin, Sparkle } from "lucide-react";
 import Marquee from "react-fast-marquee";
 import TimeFlex from "./components/Time";
+import Myanimelist from "./components/Myanimelist";
 
 const OS = Open_Sans({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
     <>
       <div className="absolute top-20 -mt-16 md:-mt-0 -ml-40 w-full rotate-[-0.4rad] text-red-500 font-bold text-5xl md:text-7xl">
         <Marquee direction="right" className="slide-in-left overflow-hidden">
-          <p>FUJATYPING</p><Sparkle size={50} className="mx-10" /><p>FJT</p><Sparkle size={50} className="mx-10" /><p>SIRAPHOP</p><Sparkle size={50} className="mx-10" /><p>フジャタイピング</p><Sparkle size={50} className="mx-10" /><p>후자타이핑</p><Sparkle size={50} className="mx-10" />
+          <p>FUJATYPING</p>
+          <Sparkle size={50} className="mx-10" />
+          <p>FJT</p>
+          <Sparkle size={50} className="mx-10" />
+          <p>SIRAPHOP</p>
+          <Sparkle size={50} className="mx-10" />
+          <p>フジャタイピング</p>
+          <Sparkle size={50} className="mx-10" />
+          <p>후자타이핑</p>
+          <Sparkle size={50} className="mx-10" />
         </Marquee>
       </div>
       <div className="w-full mt-10 md:mt-16 flex flex-col items-center justify-center p-6 mb-8 swing-in-top-fwd">
@@ -27,16 +37,43 @@ export default function Home() {
           <div className="flex flex-col md:flex-row gap-6 place-content-between px-6 py-12 text-white border-t-4 border-red-900">
             <div>
               <h1 className="font-bold text-4xl md:text-5xl">FujaTyping</h1>
-              <p className="max-w-md mt-3">3D modeler, Web developer, and Roblox scripter, Anime lover and music vibe hunter</p>
+              <p className="max-w-md mt-3">
+                3D modeler, Web developer, and Roblox scripter, Anime lover and
+                music vibe hunter
+              </p>
             </div>
-            <div className={`${OS.className} flex flex-col w-full md:ml-6 md:flex-row items-center justify-center gap-5`}>
-              <Link href={"https://github.com/FujaTyping"} className="w-full flex items-center justify-center gap-2"><Github size={18} /> Github</Link>
-              <Link href={"https://x.com/FujaTyping"} className="w-full flex items-center justify-center gap-2"><Twitter size={18} /> Twitter</Link>
-              <Link href={"mailto:contact@siraphop.me"} className="w-full flex items-center justify-center gap-2"><Mail size={18} /> Mail</Link>
-              <Link href={"https://www.linkedin.com/in/siraphop-sukchu/"} className="w-full flex items-center justify-center gap-2"><Linkedin size={18} /> Linkedin</Link>
+            <div
+              id="MainLink"
+              className={`${OS.className} flex flex-col w-full md:ml-6 md:flex-row items-center justify-center gap-5`}
+            >
+              <Link
+                href={"https://github.com/FujaTyping"}
+                className="w-full flex items-center justify-center gap-2"
+              >
+                <Github size={18} /> Github
+              </Link>
+              <Link
+                href={"https://x.com/FujaTyping"}
+                className="w-full flex items-center justify-center gap-2"
+              >
+                <Twitter size={18} /> Twitter
+              </Link>
+              <Link
+                href={"mailto:contact@siraphop.me"}
+                className="w-full flex items-center justify-center gap-2"
+              >
+                <Mail size={18} /> Mail
+              </Link>
+              <Link
+                href={"https://www.linkedin.com/in/siraphop-sukchu/"}
+                className="w-full flex items-center justify-center gap-2"
+              >
+                <Linkedin size={18} /> Linkedin
+              </Link>
             </div>
           </div>
         </div>
+        <Myanimelist />
         <TimeFlex />
         <div className="mt-10 flex gap-2">
           <div className="w-9 h-9 bg-red-600 rounded-full"></div>
